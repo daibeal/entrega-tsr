1764226f86cd4d1f02c74a1261d954985005eb40
daibeal <57094326+daibeal@users.noreply.github.com>
2021-12-01 17:42:33 +0100

    Created some files
    Added

---
+++ source-code/double-broker/broker1.js
@@ -0,0 +1,35 @@
+const zmq = require("zmq");
+let cli = [],
+  req = [];
+let total = 0,
+  w = 0;
+let sc = zmq.socket("router"); // frontend
+let sb = zmq.socket("dealer"); // backend
+sc.bind("tcp://*:" + process.argv[2]);
+sb.bind("tcp://*:8002");
+sc.on("message", (c, sep, m) => {
+  if (w == 0) {
+    cli.push(c);
+    req.push(m);
+  } else {
+    sb.send([c, "", m]);
+    w--;
+  }
+});
+sb.on("message", (c, sep, r) => {
+  if (sep == "removeWorker") {
+    w--;
+    return;
+  }
+  if (cli.length > 0) {
+    sb.send([cli.shift(), "", req.shift()]);
+  } else {
+    w++;
+  }
+  if (sep == "") {
+    sc.send([c, "", r]);
+  }
+});
+setInterval(() => {
+  console.log(cli);
+}, 200);
