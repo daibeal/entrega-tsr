02bf12bc5172b773e46b844b6b9dce0ed1ed61a4
daibeal <57094326+daibeal@users.noreply.github.com>
2021-12-01 17:50:23 +0100

    Sumamry

---
+++ source-code/double-broker/worker.js
@@ -0,0 +1,14 @@
+const zmq = require('zmq')
+let dela = zmq.socket('dealer') 
+dela.identity=process.argv[3] 
+dela.connect(process.argv[2]) 
+dela.on('message', (c,sep,msg)=> {
+    setTimeout(()=> { dela.send([c,'',process.argv[4]])
+}, 1000) })
+dela.send(['','addWorker',''])
+process.on('SIGINT', () => { 
+    dela.send(['','removeWorker','']) 
+    setTimeout(() => {
+        process.exit(0) 
+    },100)
+})
