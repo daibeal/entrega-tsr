3ce8f2d89555da5d7afd14ab4ef5ce525618f61a
daibeal <57094326+daibeal@users.noreply.github.com>
2021-12-01 17:24:13 +0100

    New files
    Added new files

---
+++ publicador-rotatorio/statistics-broker/broker.js
@@ -0,0 +1,39 @@
+const zmq = require("zmq");
+let cli = [],
+  req = [],
+  workers = [],
+  answeredReq = {};
+let total = 0;
+let sc = zmq.socket("router"); // frontend
+let sw = zmq.socket("router"); // backend
+sc.bind("tcp://*:" + process.argv[2]);
+sw.bind("tcp://*:" + process.argv[3]);
+sc.on("message", (c, sep, m) => {
+  if (workers.length == 0) {
+    cli.push(c);
+    req.push(m);
+  } else {
+    sw.send([workers.shift(), "", c, "", m]);
+  }
+});
+sw.on('message',(w,sep1,c,sep2,r)=> {
+    if (c=='') {
+        workers.push(w); 
+        answeredReq[w] = 0;
+         return
+        }
+        if (cli.length>0) {
+        sw.send([w,'', cli.shift(),'',req.shift()])
+        } else {
+        workers.push(w)
+        } 
+        answeredReq[w]++ 
+        sc.send([c,'',r]) 
+        total++
+})
+setInterval(() => {
+    console.log("Total peticiones atendidas: " + total) 
+    for (let i in answeredReq) {
+    console.log('Peticiones atendidas por ' + i +': ' + answeredReq[i])
+    }
+}, 5000)
